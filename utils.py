6887bfc8a85533909d9a5b5a8ab13e348e3b43c4
Nick Kapetanas <nkapetanas@outlook.com>
2020-01-26 21:23:59 +0200

    added initial implementation of bucketizer

--- utils.py
+++ utils.py
@@ -1,4 +1,20 @@
+import numpy as np
+
 
 def field_name_changer(df, columns_name_list):
     for value in columns_name_list[:-1]:
         df[value] = df[value].apply(lambda x: str(x) + value)
+
+
+def bucketizer(df, columns_name_list):
+    for value in columns_name_list[:-1]:
+        column_max = df[value].max()
+        column_min = df[value].min()
+        stddev = df[value].std()
+
+        number_of_buckets = 1
+        if stddev != 0:
+            number_of_buckets = ((column_max - column_min) // (stddev))
+
+        buckets = np.arange(number_of_buckets, dtype=np.float).tolist()
+        # TODO add the binning
